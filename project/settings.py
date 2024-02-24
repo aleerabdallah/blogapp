1bc421d579eadcb2d2af8d8a093ab3721733efc6
aleerabdallah <aleerabdallah7@gmail.com>
2024-02-25 01:22:16 +0200

    Initial commit

--- project/settings.py
+++ project/settings.py
@@ -163,6 +163,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'img/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 
