71e438c8716b538b4a5fa2bc15adc2b5ed12d8c8
aleerabdallah <aleerabdallah7@gmail.com>
2024-02-24 23:31:26 +0200

    Configuring ALLOWED_HOSTS in settings.py

--- project/settings.py
+++ project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
